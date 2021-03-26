f0e3ea83a15d690b544b989926d2ce3b1bf36008
adrian.petre <adrian.petre@servexternos.gruposantander.com>
2021-03-26 08:47:08 +0000

    configured webpack-dev-server

--- README.md
+++ README.md
@@ -8,3 +8,21 @@ https://docs.npmjs.com/cli/v7/configuring-npm/package-json
 ```bash
 npm run build
 ```
+
+# libraries
+https://www.npmjs.com/package/font-awesome-webpack
+
+# Using webpack-dev-middleware
+The publicPath will be used within our server script as well in order to make sure files are served correctly on http://localhost:3000. We'll specify the port number later. The next step is setting up our custom express server.js.
+
+# Adjusting Your Text Editor
+### VSCode: settings.json
+```json
+{
+  "editor.tabSize": 2,
+  "prettier.tabWidth": 2,
+  "prettier.endOfLine": "lf",
+  "files.eol": "\n",
+  "files.autoSave": "off"
+}
+```
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,49 @@
 const path = require("path");
+const json5 = require("json5");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-	entry: "./src/index.js",
-	output: {
-		filename: "main.js",
-		path: path.resolve(__dirname, "dist"),
+	mode: "development",
+	entry: {
+		index: "./src/index.js",
+		print: "./src/print.js",
 	},
-	mode: 'development',
+	devtool: "inline-source-map",
+	devServer: {
+		contentBase: "./dist",
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: "Development",
+		}),
+	],
+	output: {
+		filename: "[name].bundle.js",
+		path: path.resolve(__dirname, "dist"),
+		clean: true,
+		publicPath: '/',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader"],
+			},
+			{
+				test: /\.(ico|png|svg|jpg|jpeg|gif)$/i,
+				type: "asset/resource",
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/i,
+				type: "asset/resource",
+			},
+			{
+				test: /\.json5$/i,
+				type: "json",
+				parser: {
+					parse: json5.parse,
+				},
+			},
+		],
+	},
 };
