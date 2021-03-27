409524f676a6e0a54a0a46f4a1f27511582c92b3
adrhc <adrhc@outlook.com>
2021-03-27 12:30:46 +0200

    postcss.config.js: postcss-preset-env, postcss-short

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  plugins: [require("autoprefixer")],
+  // You can specify any options from https://postcss.org/api/#processoptions here
+  // parser: 'sugarss',
+  plugins: [
+    // Plugins for PostCSS
+    ["postcss-short", { prefix: "x", skip: "_" }],
+    "postcss-preset-env",
+  ],
 };
