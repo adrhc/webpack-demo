f456ff04b49c0e1b8c1759d72f70d75f94bfd473
adrian.petre <adrian.petre@servexternos.gruposantander.com>
2021-03-29 17:07:45 +0100

    from endava

--- webpack.config.js
+++ webpack.config.js
@@ -6,69 +6,69 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const devMode = process.env.NODE_ENV !== "production";
 
 module.exports = {
-  mode: process.env.NODE_ENV,
-  entry: {
-    index: "./src/index.js",
-  },
-  output: {
-    // filename: "[name].bundle.js",
-    filename: "[name].[contenthash].bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-    publicPath: "",
-  },
-  optimization: {
-    // runtimeChunk: "single",
-    splitChunks: {
-      // include all types of chunks
-      chunks: "all",
-    },
-  },
+	mode: process.env.NODE_ENV,
+	entry: {
+		index: "./src/index.js",
+	},
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		// filename: "[name].bundle.js",
+		filename: devMode ? "[name].bundle.js" : "[name].[contenthash].bundle.js",
+		clean: true,
+		publicPath: "",
+	},
+	optimization: {
+		// runtimeChunk: "single",
+		splitChunks: {
+			// include all types of chunks
+			chunks: "all",
+		},
+	},
+	watchOptions: {
+		aggregateTimeout: 500,
+		ignored: /node_modules/,
+	},
 	devtool: devMode ? "inline-source-map" : "source-map",
-  devServer: {
-    contentBase: "./dist",
-    port: 1234,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(sa|sc|c)ss$/,
-        use: [
-          devMode ? "style-loader" : MiniCssExtractPlugin.loader,
-          "css-loader",
-          "postcss-loader",
-          "sass-loader",
-        ],
-      },
-      {
-        test: /\.(ico|png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
-      },
-      {
-        test: /\.json5$/i,
-        type: "json",
-        parser: {
-          parse: json5.parse,
-        },
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Development",
-    }),
-    new MiniCssExtractPlugin({
-      filename: devMode ? "[name].css" : "[name].[contenthash].css",
-      chunkFilename: devMode ? "[id].css" : "[id].[contenthash].css",
-    }),
-    /* new webpack.SourceMapDevToolPlugin({
+	devServer: {
+		contentBase: "./dist",
+		port: 1234,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(sa|sc|c)ss$/,
+				use: [devMode ? "style-loader" : MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "sass-loader"],
+			},
+			{
+				test: /\.(ico|png|svg|jpg|jpeg|gif)$/i,
+				type: "asset/resource",
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/i,
+				type: "asset/resource",
+			},
+			{
+				test: /\.json5$/i,
+				type: "json",
+				parser: {
+					parse: json5.parse,
+				},
+			},
+		],
+	},
+	plugins: [
+		new webpack.ProgressPlugin(),
+		new HtmlWebpackPlugin({
+			title: "Development",
+		}),
+		new MiniCssExtractPlugin({
+			filename: devMode ? "[name].css" : "[name].[contenthash].css",
+			chunkFilename: devMode ? "[id].css" : "[id].[contenthash].css",
+		}),
+		/* new webpack.SourceMapDevToolPlugin({
       filename: "sourcemaps/[file].map",
       publicPath: "",
       fileContext: "public",
     }), */
-  ],
+	],
 };
